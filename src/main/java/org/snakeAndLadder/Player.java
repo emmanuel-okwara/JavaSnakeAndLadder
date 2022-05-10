e476fb959eb5f2ac52d654bdcf19c706b3a6a675
kmashigo021 <mic@pop-os.localdomain>
2022-05-10 10:57:18 +0200

    added player class

---
+++ src/main/java/org/snakeAndLadder/Player.java
@@ -0,0 +1,20 @@
+package org.snakeAndLadder;
+
+public class Player {
+    private int position;
+    private int turns;
+
+    public void Player(){
+        this.position = 0;
+        this.turns = 1;
+    }
+
+    public void move(int steps){
+        boolean validMove= this.position + steps <= 100;
+        if (validMove){
+            this.position += steps;
+        }
+        
+    }
+    
+}

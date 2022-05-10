576bec22b82f5e669f4421b4270b6832b731fd8e
kmashigo021 <mic@pop-os.localdomain>
2022-05-10 11:24:27 +0200

    added test package

---
+++ src/test/java/org/snakeAndLadder/Tests.java
@@ -0,0 +1,18 @@
+package org.snakeAndLadder;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+
+public class Tests {
+
+    @Test
+    void TesRoll(){
+        Dice dice = new Dice();
+        int roll = dice.roll();
+        Boolean isInRange = 1 <= roll && roll <= 6;
+        assertTrue(isInRange);
+    }
+    
+}
